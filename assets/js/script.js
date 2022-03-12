f4993f0d54d8c0521d309fde587de68a1afda4a5
lisavi <lisavi_28@yahoo.com.mx>
2022-03-12 11:46:16 -0600

    style

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,16 +4,14 @@ var buttonEl = document.querySelector("#start");
 var sectionEl = document.querySelector ("#questions-container");
 var headerEl = document.querySelector(".welcome-box");
 var timerEl = document.querySelector("#init");
-var opt1El = document.querySelector("#btn1");
-var opt2El = document.querySelector("#btn2");
-var opt3El = document.querySelector("#btn3");
-var opt4El = document.querySelector("#btn4");
 var optionsEl = document.querySelector("#answer-buttons");
 var alertsEl = document.querySelector("#alerts");
 var inputEl = document.querySelector(".buttons");
 var i = 0;
 var corrects = 0;
 var timeLeft = 50;
+
+//start button function
 
 buttonEl.addEventListener("click", function() {
     buttonEl.classList.add("hide")
@@ -27,10 +25,10 @@ buttonEl.addEventListener("click", function() {
                       document.getElementById("btn1").innerHTML = questions[0].options[0];
                       document.getElementById("btn2").innerHTML = questions[0].options[1];
                       document.getElementById("btn3").innerHTML = questions[0].options[2];
-                      document.getElementById("btn4").innerHTML = questions[0].options[3];
-                      
-                        
+                      document.getElementById("btn4").innerHTML = questions[0].options[3];                       
 });
+
+//  recurrent questions quiz function
 
 optionsEl.addEventListener("click", function(event) {
   var correctans = event.target;
@@ -45,61 +43,49 @@ alertsEl.innerHTML="";
         timeLeft=timeLeft-10;
       }
 
-
-if (i < (questions.length-1) ) {
+      if (i < (questions.length-1) ) {
                i = i+1;
                     document.getElementById("question").innerHTML = questions[i].question;
                     document.getElementById("btn1").innerHTML = questions[i].options[0];
                     document.getElementById("btn2").innerHTML = questions[i].options[1];
                     document.getElementById("btn3").innerHTML = questions[i].options[2];
                     document.getElementById("btn4").innerHTML = questions[i].options[3];
-}
+      }
 
-
-else {
-  finish();
-}
+      else {
+          finish();
+      }
 });
+
+// finish function 
+
  var finish = function() {
    window.alert("you got  "+corrects +"correct answers");
    sectionEl.classList.add("hide")
    timerEl.classList.add("hide")
    document.getElementById("alerts").innerHTML = "Your final score is " + (corrects *25)+" Add your name below for the database";
    inputEl.classList.remove("hide")
-
  }
 
-// Timer that counts down from 5
-function timer() {
-  
+// Timer function
 
-  // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+function timer() {
   var timeInterval = setInterval(function () {
-    // As long as the `timeLeft` is greater than 1
     if (timeLeft > 1) {
-      // Set the `textContent` of `timerEl` to show the remaining seconds
-      timerEl.textContent = timeLeft + ' seconds remaining';
-      // Decrement `timeLeft` by 1
+           timerEl.textContent = timeLeft + ' seconds remaining';
       timeLeft--;
     } else if (timeLeft === 1) {
-      // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
       timerEl.textContent = timeLeft + ' second remaining';
       timeLeft--;
     } else {
-      // Once `timeLeft` gets to 0, set `timerEl` to an empty string
       timerEl.textContent = '';
-      // Use `clearInterval()` to stop the timer
-      clearInterval(timeInterval);
-      // Call the `displayMessage()` function
-      displayMessage();
+            clearInterval(timeInterval);
+       displayMessage();
     }
   }, 1000);
 }
 
-
-
-// section questions
-
+// questions section
 
   var questions= [
 {
